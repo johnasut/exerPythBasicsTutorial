9185b4b6c74c9e2061174ce5cc0e9fee318c5d40
johnasut <john.saunders@pharm.utah.edu>
2020-09-12 18:54:40 -0600

    Python Basics Tutorial Assignment

---
+++ mygame/draw.py
@@ -0,0 +1,11 @@
+def draw_game():
+    clear_screen(main_screen)
+    ...
+
+def clear_screen(screen):
+    ...
+
+class Screen():
+    ...
+
+main_screen = Screen()
---
+++ mygame/game.py
@@ -0,0 +1,14 @@
+if visual_mode:
+    import draw_visual as draw
+else:
+    import draw_textual as draw
+
+def play_game():
+    ...
+
+def main():
+    result = play_game()
+    draw.draw_game(result)
+
+if __name__ == '__main__':
+    main()
